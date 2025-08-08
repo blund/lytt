1edfa2edf65c5f4f443c7956d63ceb3c4ec722c8
Børge Lundsaunet <mail@blund.site>
2025-08-09 00:10:24 +0200

    fix up read_file a bit

--- bl.h
+++ bl.h
@@ -70,35 +70,25 @@ int read_file(const char* filename, char** out) {
     size_t size;
     char* data = 0;
 
-    struct stat st;
-    if (stat(filename, &st) != 0) {
-        fprintf(stderr, "Failed to access file '%s' for file size, errno = %d (%s)\n",
-                filename, errno, strerror(errno));
-        abort();
-    }
-    size = (size_t)(st.st_size);
+    FILE* fp = fopen(filename, "r");
+    if (fp == 0) return 0;
 
+    struct stat st;
+    if (stat(filename, &st) != 0) return 0;
+
+    size = (size_t)(st.st_size);
     data = (char *)malloc(sizeof(char) * size + 1);
 
     // Make sure we terminate this scary C string :)
     data[size] = '\0';
 
-    FILE* fp = fopen(filename, "r");
-    if (fp == 0) {
-        fprintf(stderr, "Failed to open file '%s' for read, errno = %d (%s)\n",
-                filename, errno, strerror(errno));
-        abort();
-    }
-
     size_t bytesRead = fread(data, sizeof(char), size, fp);
     assert(bytesRead == size, "incorrect byte count read");
 
-    {
-        // just to sanity check we have reached eof
-        uint8_t _c;
-        assert(fread(&_c, sizeof(uint8_t), 1 /*numElements*/, fp) == 0, "");
-        assert(feof(fp), "");
-    }
+    // just to sanity check we have reached eof
+    uint8_t _c;
+    assert(fread(&_c, sizeof(uint8_t), 1 /*numElements*/, fp) == 0, "");
+    assert(feof(fp), "");
 
     fclose(fp);
 
